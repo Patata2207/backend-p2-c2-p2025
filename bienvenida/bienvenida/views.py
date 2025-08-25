a270fd95f72e09ac56426b6c0e605a60026edef3
Matias Ignacio Herrera Orrego <matias.herrera62@inacapmail.cl>
2025-08-25 16:13:18 -0400

    Revert "mensaje de prueba"
    This reverts commit ba2bfb8761e886cc8981c3a18edc6d355b0bb289.

--- bienvenida/bienvenida/views.py
+++ bienvenida/bienvenida/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 def inicio(request):
     return HttpResponse("Hola mundo desde Django")
 
-#mensaje de pruea
+
 def mostrar_bienvenida(request):
     tu_nombre = "Matias Herrera"
     return HttpResponse(f"Bienvenidos a mi primera app Django, {tu_nombre}")
