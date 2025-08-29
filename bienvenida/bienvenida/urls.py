9532a7da59eb3e72453ea1b1829b6b495701a158
Matias Ignacio Herrera Orrego <matias.herrera62@inacapmail.cl>
2025-08-29 16:38:07 -0400

    feat: mi primera plantilla
    ╰(*°▽°*)╯

--- bienvenida/bienvenida/urls.py
+++ bienvenida/bienvenida/urls.py
@@ -21,5 +21,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('inicio/',views.mostrar_bienvenida)
+    path('inicio/',views.mostrar_bienvenida,name = "Bienvenida"),
+    path('productos/',views.lista_productos,name = "Productos" )
 ]
